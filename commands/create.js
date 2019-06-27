ca0680ab2116f509ada80efd1026d2e095c4f6f3
Umberto Barros <umberto@salvus.me>
2019-06-26 21:26:26 -0300

    [master] | templates and file generator

--- commands/create.js
+++ commands/create.js
@@ -1,9 +1,54 @@
 const fs = require('fs');
 
 module.exports = (commands, params) => {
-    fs.writeFile(`./dale/${params.name}.js`, 'This is my text', 
-        err => {
-            if (err) throw err;
-            console.log(`${params.name}.js is Created Successfully`);
-        });
+
+    let path = commands[0].split('/');
+
+    let foldersPath = path.slice(0, path.length - 1).join('/');    
+    
+    mkdir_p(foldersPath);
+
+    const template = fs.readFileSync(__dirname + '/../templates/component-class').toString();
+
+    fs.writeFile(commands[0], template, err => console.log(err))
+
+}
+
+/**
+ * @from https://brunopedro.com/2010/12/15/recursive-directory-nodejs/
+ * 
+ * Offers functionality similar to mkdir -p
+ *
+ * Asynchronous operation. No arguments other than a possible exception
+ * are given to the completion callback.
+ */
+function mkdir_p(path, mode = 0777, callback, position = 0) {
+    parts = require('path').normalize(path).split('/');
+
+    if (position >= parts.length) {
+        if (callback) {
+            return callback();
+        } else {
+            return true;
+        }
+    }
+
+    var directory = parts.slice(0, position + 1).join('/');
+    fs.stat(directory, function(err) {
+        if (err === null) {
+            mkdir_p(path, mode, callback, position + 1);
+        } else {
+            fs.mkdir(directory, mode, function (err) {
+                if (err) {
+                    if (callback) {
+                        return callback(err);
+                    } else {
+                        throw err;
+                    }
+                } else {
+                    mkdir_p(path, mode, callback, position + 1);
+                }
+            })
+        }
+    })
 }
