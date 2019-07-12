aa0cadbfb47182638d32e76c4b9845ef1ee4ebb5
UmbertoB <umbertobarrosf@gmail.com>
2019-07-11 23:54:43 -0300

    [master] | tests

--- tests/create-component.test.js
+++ tests/create-component.test.js
@@ -1,12 +1,12 @@
 var fs = require('fs');
 var cp = require('child_process');
-var utils = require('./utils');
+var { rimraf, execute } = require('./utils');
 
 describe('CREATE COMPONENT SERVICE', () => {
 
     it('creates a component', () => {
 
-        cp.execSync('uct create test', { cwd: __dirname });
+        execute('uct create test');
 
         let component, styles, tests;
         
@@ -18,7 +18,7 @@ describe('CREATE COMPONENT SERVICE', () => {
             //
         }
 
-        utils.rimraf(__dirname + '/Test');
+        rimraf(__dirname + '/Test');
 
         // component, style and test files created
         expect(!!component && !!styles && !!tests).toBeTruthy();
@@ -27,7 +27,7 @@ describe('CREATE COMPONENT SERVICE', () => {
 
     it('creates a component without tests', () => {
 
-        cp.execSync('uct create test --spec false', { cwd: __dirname });
+        execute('uct create test --spec false');
 
         let component, styles, tests;
         
@@ -39,7 +39,7 @@ describe('CREATE COMPONENT SERVICE', () => {
             //
         }
 
-        utils.rimraf(__dirname + '/Test');
+        rimraf(__dirname + '/Test');
 
         // component and style files created
         expect(!!component && !!styles && !!!tests).toBeTruthy();
@@ -48,7 +48,7 @@ describe('CREATE COMPONENT SERVICE', () => {
 
     it('creates a component without stylesheets', () => {
 
-        cp.execSync('uct create test --styles false', { cwd: __dirname });
+        execute('uct create test --styles false');
 
         let component, styles, tests;
         
@@ -60,7 +60,7 @@ describe('CREATE COMPONENT SERVICE', () => {
             //
         }
 
-        utils.rimraf(__dirname + '/Test');
+        rimraf(__dirname + '/Test');
 
         // component and test files created
         expect(!!component && !!!styles && !!tests).toBeTruthy();
